62ec9570180e03dc3c24e5a72c4feb8e80f23694
Jesse Lott <jesse.e.lott@outlook.com>
2017-01-20 20:20:56 -0500

    Add files via upload

---
+++ Models/Pitcher.cs
@@ -0,0 +1,59 @@
+﻿namespace PlayerLibrary.Models
+{
+    public class Pitcher : Player
+    {
+        //pitching stats
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Saves { get; set; }
+        public int GamesStarted { get; set; }
+        public int CompleteGames { get; set; }
+        public int ShutOuts { get; set; }
+        public int InningsPitched { get; set; }
+        public int HitsAllowed { get; set; }
+        public int RunsAllowed { get; set; }
+        public int EarnedRunsAllowed { get; set; }
+        public int HomeRunsAllowed { get; set; }
+        public int Ks { get; set; }
+        public int WalksIssued { get; set; }
+        public int HitByPitches { get; set; }
+        public int WildPitches { get; set; }
+
+        //pitching stats that require formulas
+        public float WinningPct
+        {
+            get
+            {
+                return (float)Wins /
+                    (float)Wins +
+                    (float)Losses;
+            }
+        }
+        public float EarnedRunAvg
+        {
+            get
+            {
+                return 9 * ((float)EarnedRunsAllowed /
+                    (float)InningsPitched);
+
+            }
+        }
+        public float KsPerNineInnings
+        {
+            get
+            {
+                return (float)Ks / 9;
+            }
+        }
+        public float WalksAndHitsInningPitched
+        {
+            get
+            {
+                return (float)Walks +
+                    (float)Ks /
+                    (float)InningsPitched;
+            }
+        }
+
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlayerLibrary.Models
+{
+    public class Player
+    {
+        //admin properties
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int PersonId { get; set; }
+        [StringLength(25)]
+        public string TeamAssigned { get; set; }
+        [StringLength(2)]
+        public string Position { get; set; }
+
+        //vital statistics
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string LastName { get; set; }
+        public string HomeTownAndState { get; set; }
+        public string FavoritePlayer { get; set; }
+        public DateTime WhenStarted { get; set; }
+
+        //statistic properties
+        public int GamesPlayed { get; set; }
+        
+        //hitting stats
+        public int AtBats { get; set; }
+        public int Hits { get; set; }
+        public int Runs { get; set; }
+        public int RunsBattedIn { get; set; }
+        public int HomeRuns { get; set; }
+        public int StolenBases { get; set; }
+        public int CaughtStealing { get; set; }
+        public int SacrificeBunts { get; set; }
+        public int SacrificeFlies { get; set; }
+        public int Doubles { get; set; }
+        public int Triples { get; set; }
+        public int StrikeOuts { get; set; }
+        public int Walks { get; set; }
+        //batting stats with formulas
+        public float SluggingPct
+        {
+            get { return Hits + (float)(2 * Doubles) + 
+                    (float)(3* Triples) + 
+                    (float)(4 * HomeRuns) / 
+                    (float)AtBats;
+            }
+        }
+        public float OnBaseAvg
+        {
+            get { return 
+                    (float)Hits + 
+                    (float)Walks /
+                    (float)AtBats + 
+                    (float)Hits + 
+                    (float)SacrificeBunts + 
+                    (float)SacrificeFlies;
+            }
+        }
+        public float OnBasePlusSlugging
+        {
+            get
+            {
+                return OnBaseAvg + 
+                    SluggingPct;
+            }
+        }
+        public float BattingAvg
+        {
+            get
+            {
+                return (float)Hits / 
+                    (float)AtBats;
+            } 
+        }
+        
+        //fielding stats
+        public int PutOuts { get; set; }
+        public int Assists { get; set; }
+        public int Errors { get; set; }
+
+        public int Chances
+        {
+            get { return PutOuts + Assists + Errors; }
+        }
+
+        //fielding stats that require formulas
+        public float FieldingPct
+        {
+            get
+            {
+                return (float) Errors /
+                       (float) Chances;
+            }
+        }
+
+    }
+}
